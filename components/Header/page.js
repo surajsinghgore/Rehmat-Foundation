863461692c230fea0048f5aad59400cf7a6878b9
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2023-12-19 18:11:13 +0530

    feat header added

---
+++ components/Header/page.js
@@ -0,0 +1,48 @@
+import style from "./page.module.css";
+import logo from "../../app/icon.png";
+import Image from "next/legacy/image";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div className={style.Header}>
+      {/* Header Logo */}
+      <div className={style.logo}>
+        <div className={style.image}>
+          <Link href={"/"}>
+            <Image src={logo} alt="Logo" layout="fill" />
+          </Link>
+        </div>
+        <div className={style.heading}>
+          R.F. <span>CHD</span>
+        </div>
+      </div>
+      {/* links of header */}
+      <div className={style.links}>
+        <li className={style.activePage}>
+          <Link href="/">HOME</Link>
+        </li>
+        <li>
+          <Link href="">ABOUT</Link>
+        </li>
+        <li>
+          <Link href="">GALLERY</Link>
+        </li>
+        <li>
+          <Link href="">EVENTS</Link>
+        </li>
+        <li>
+          <Link href="">CONTACT</Link>
+        </li>
+        <li>
+          <Link href="">MEMBERS</Link>
+        </li>
+      </div>
+
+      {/* donate button */}
+      <div className={style.donateButton}>
+        <button>DONATE</button>
+      </div>
+    </div>
+  );
+}
