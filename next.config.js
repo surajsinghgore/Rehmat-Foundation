9ff6a6593ef08fc30b0b4335441eca3bbf2567a8
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-01-12 19:46:44 +0530

    seo fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    
-}
-
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+      const headers = [];
+      if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+        headers.push({
+          headers: [
+            {
+              key: 'X-Robots-Tag',
+              value: 'noindex',
+            },
+          ],
+          source: '/:path*',
+        });
+      }
+      return headers;
+    },
+   };
