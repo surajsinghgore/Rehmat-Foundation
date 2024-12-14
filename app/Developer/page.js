4ea9b7d60edbca60898b634aa96d63c0b4d4dd87
AMANDEEP SINGH MAJITHIA <102940833+Ammyasl2003@users.noreply.github.com>
2024-12-14 22:52:21 +0530

    Update page.js

--- app/Developer/page.js
+++ app/Developer/page.js
@@ -17,6 +17,8 @@ export default function page() {
 
       
       <p><span>Amandeep Singh </span><a href="https://www.linkedin.com/in/amandeep-singh-majithia-2102a6258/" target='_blank '>Click Here to Contact</a></p>
+       <p><span>Tushar Negi </span><a href="https://www.linkedin.com/in/tushar-negi-786571317/" target='_blank '>Click Here to Contact</a></p>
+
 
 
       </div>
